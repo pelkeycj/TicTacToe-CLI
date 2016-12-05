34714e649e39d190e41185269ef4812ac183d2c1
Connor Pelkey <Pelkey.c@husky.neu.edu>
2016-12-04 22:36:00 -0500

    adds instruction display

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ import random
 
 def main():
     board = Board()
-    
+    printInstructions()
+
+    while True:
+
+def printInstructions():
+    print("Tic-Tac-Toe")
+    print("____________")
+    print("Choose coordinates (row,column) when prompted.")
+    print("     eg. 20 , 00, etc.")
+    print("Good Luck!")
+
 
 if __name__ == "__main__":
     main()
