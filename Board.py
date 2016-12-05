71f93e6e9766774189df62adad3133ae55209fc0
Connor Pelkey <Pelkey.c@husky.neu.edu>
2016-12-04 23:13:14 -0500

    minor bug fixes, adds missing diagonal end state

--- Board.py
+++ Board.py
@@ -1,10 +1,12 @@
+import random
+
 class Board():
     def __init__(self):
         self.grid = [[" " for x in range(3)] for y in range(3)]
 
     def reset(self):
         '''Resets to board to blank grid'''
-        self.grid = [["" for x in range(3)] for y in range(3)]
+        self.grid = [[" " for x in range(3)] for y in range(3)]
 
     def showBoard(self):
         '''Display board'''
@@ -25,16 +27,16 @@ class Board():
     def isValid(self, row, col):
         '''Returns True if coordinates are valid
             and available'''
-            if row > 2 or col > 2:
-                return False
-            elif grid[row][col] is not " ":
-                return False
-            else:
-                return True
+        if row > 2 or col > 2:
+            return False
+        elif self.grid[row][col] is not " ":
+            return False
+        else:
+            return True
 
     def setValue(self, row, col, symbol):
         '''Sets symbol if valid coordinates'''
-            self.grid[row][col] = symbol
+        self.grid[row][col] = symbol
 
     def setRand(self):
         '''Places 'O' at random available location'''
@@ -44,24 +46,28 @@ class Board():
             row = random.randint(0,2)
             col = random.randint(0,2)
 
-            if isValid(row,col):
-                setValue(row, col, "O")
+            if self.isValid(row,col):
+                self.setValue(row, col, "O")
                 return
 
     def isWon(self):
         '''Determines whether game has been won'''
         #check horizontal
         for row in self.grid:
-            if row[0] is row[1] is row[2] and row[0] is not " ":
+            if row[0] is row[1] is row[2] and row[0] is not" ":
                 return True
         #check vertical
-        for i in range(3):
-            if self.grid[0][col] is
-              self.grid[1][col] is
-              self.grid[2][col]
-              and self.grid[0][col] is not " ":
+        for col in range(3):
+            if self.grid[0][col] is \
+              self.grid[1][col] is \
+              self.grid[2][col] and\
+              self.grid[0][col] is not " ":
                return True
 
-        if self.grid[0][0] is self.grid[1][1] is self.grid[2][2]
+        if self.grid[0][0] is self.grid[1][1] is self.grid[2][2] \
             and self.grid[0][0] is not " ":
+            return True
+
+        if self.grid[2][0] is self.grid[1][1] is self.grid[0][2] \
+            and self.grid[2][0] is not " ":
             return True
