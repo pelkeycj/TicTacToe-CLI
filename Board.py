07aceca169b340b386b634a4ac526f2771e50600
Connor Pelkey <Pelkey.c@husky.neu.edu>
2016-12-04 22:13:29 -0500

    adds position checking and setting

--- Board.py
+++ Board.py
@@ -2,12 +2,9 @@ class Board():
     def __init__(self):
         self.grid = [[" " for x in range(3)] for y in range(3)]
 
-    def grid(self):
-        return self.grid
-
     def showBoard(self):
         '''Display board'''
-        print(" A  B  C")
+        print(" 0  1  2")
         for row in range(3):
             line = str(row) + " "
             for col in range(3):
@@ -19,3 +16,30 @@ class Board():
             print(line)
             if row is not 2:
                 print("  - - -")
+
+
+    def isValid(self, row, col):
+        '''Returns True if coordinates are valid
+            and available'''
+            if row > 2 or col > 2:
+                return False
+            elif grid[row][col] is not " ":
+                return False
+            else:
+                return True
+
+    def setValue(self, row, col, symbol):
+        '''Sets symbol if valid coordinates'''
+            self.grid[row][col] = symbol
+
+    def setRand(self):
+        random.seed()
+
+        while True:
+            row = random.randint(0,2)
+            col = random.randint(0,2)
+
+            if isValid(row,col):
+                setValue(row, col, "O")
+                return
+                
